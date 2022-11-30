db9e9475e61d41dbfc3d498738fa7c028f6d92fd
Ricardo Boraure <r.boraure@kation.com.do>
2022-11-30 19:52:48 -0400

    Update about me

--- pages/about.tsx
+++ pages/about.tsx
@@ -5,18 +5,18 @@ import clsx from 'clsx'
 
 import { Container } from 'components/Container'
 import {
-  TwitterIcon,
-  InstagramIcon,
   GitHubIcon,
   LinkedInIcon,
 } from 'components/SocialIcons'
 import portraitImage from 'images/portrait.jpg'
+import { Mail } from 'components/icon/Mail'
 
 function SocialLink({ className, href, children, icon: Icon }) {
   return (
     <li className={clsx(className, 'flex')}>
       <Link
         href={href}
+        target="_blank"
         className="group flex text-sm font-medium text-zinc-800 transition hover:text-blue-500 dark:text-zinc-200 dark:hover:text-blue-500"
       >
         <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-blue-500" />
@@ -26,16 +26,6 @@ function SocialLink({ className, href, children, icon: Icon }) {
   )
 }
 
-function MailIcon(props) {
-  return (
-    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
-      <path
-        fillRule="evenodd"
-        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
-      />
-    </svg>
-  )
-}
 
 export default function About() {
   return (
@@ -61,51 +51,34 @@ export default function About() {
           </div>
           <div className="lg:order-first lg:row-span-2">
             <h1 className="text-4xl font-bold tracking-tight text-zinc-800 dark:text-zinc-100 sm:text-5xl">
-            I’m Ricardo Boraure. I live in Dominican Republic, I’m deeply in love with Web Development
+            I’m Ricardo Boraure, I’m deeply in love with Web Development
             </h1>
             <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
               <p>
-                I’ve loved making things for as long as I can remember, and
-                wrote my first program when I was 6 years old, just two weeks
-                after my mom brought home the brand new Macintosh LC 550 that I
-                taught myself to type on.
+                I’m a software developer based on Dominican Republic, born in Venezuela, I’m focused on frontend development, a self-taught learner, with a huge passion for delivering smooth and useful websites, I’m so happy for giving web users the best experience they can have.
               </p>
               <p>
-                The only thing I loved more than computers as a kid was space.
-                When I was 8, I climbed the 40-foot oak tree at the back of our
-                yard while wearing my older sister’s motorcycle helmet, counted
-                down from three, and jumped — hoping the tree was tall enough
-                that with just a bit of momentum I’d be able to get to orbit.
+                I have a passion for Frontend technologies like <strong>React, Nextjs, Javascript</strong>, and <strong>JamStack</strong> ecosystem, for example, <strong>headless CMS</strong>. I love to find useful solutions and discover new technologies. I enjoy sharing my knowledge and have feedback about code and solutions.
               </p>
               <p>
-                I spent the next few summers indoors working on a rocket design,
-                while I recovered from the multiple surgeries it took to fix my
-                badly broken legs. It took nine iterations, but when I was 15 I
-                sent my dad’s Blackberry into orbit and was able to transmit a
-                photo back down to our family computer from space.
-              </p>
-              <p>
-                Today, I’m the founder of Planetaria, where we’re working on
-                civilian space suits and manned shuttle kits you can assemble at
-                home so that the next generation of kids really <em>can</em>{' '}
-                make it to orbit — from the comfort of their own backyards.
+                I care about my manners and how I communicate my thoughts to my colleagues. I’m also a cooperative teammate, that looks for team improvement in order to get the best result we can achieve.
               </p>
             </div>
           </div>
           <div className="lg:pl-20">
             <ul role="list">
-              <SocialLink href="" icon={GitHubIcon} className="mt-4">
+              <SocialLink href="https://github.com/RBoraurito" icon={GitHubIcon} className="mt-4">
                 Follow on GitHub
               </SocialLink>
-              <SocialLink href="#" icon={LinkedInIcon} className="mt-4">
+              <SocialLink href="https://www.linkedin.com/in/rboraure/" icon={LinkedInIcon} className="mt-4">
                 Follow on LinkedIn
               </SocialLink>
               <SocialLink
-                href="mailto:spencer@planetaria.tech"
-                icon={MailIcon}
+                href="mailto:ricardoboraure@gmail.com"
+                icon={Mail}
                 className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
               >
-                spencer@planetaria.tech
+                ricardoboraure@gmail.com
               </SocialLink>
             </ul>
           </div>
