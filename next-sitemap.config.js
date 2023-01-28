88505af6c55d86cc407778f0db5bab13109f8dd9
Ricardo Boraure <r.boraure@kation.com.do>
2023-01-28 18:31:54 -0400

    🔧 Update env variable

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: process.env.VERCEL_URL || 'https://rboraure.dev',
+  siteUrl: process.env.URL || 'https://rboraure.dev',
   generateRobotsTxt: true, // (optional)
   // ...other options
 }
